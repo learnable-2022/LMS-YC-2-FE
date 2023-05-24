645774ef2e26a746d21826056ce4b3d2c8596541
bukason20 <chukwuebukaedward20@gmail.com>
2023-05-25 00:18:47 +0100

    added a route for the admin login page

--- src/routes/Routy.jsx
+++ src/routes/Routy.jsx
@@ -5,6 +5,7 @@ import {
     RouterProvider
       } from "react-router-dom";
 import StudentSidebar from "../components/StudentSideBar/StudentSidebar";
+import AdminLogin from "../pages/AdminLogin/AdminLogin";
 import LandingPage from "../pages/LandingPage/LandingPage";
 import ParentLogin from "../pages/ParentLogin/ParentLogin";
 import SignUp from "../pages/SignUp/SignUp";
@@ -25,9 +26,11 @@ import StudentDashboard from "../pages/StudentDashboard/StudentDashboard";
              
               {/* LOGIN PAGE*/}
               <Route path = "/login" element = {<ParentLogin />} />
+              <Route path = "/adminLogin" element = {<AdminLogin />}/>
 
               {/* SIGN UP PAGE */}
               <Route path = "/signup" element = {<SignUp />} />
+
 
               {/* ADMIN PAGE */}
           </>
