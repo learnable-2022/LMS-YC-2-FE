3895d2c8a948d09f94e59b0459c7a53b896b862c
Ifunanyagits <iloifunanya18@gmail.com>
2023-05-24 14:50:03 +0100

    Modified parentLogin folder

--- src/pages/ParentLogin/ParentLogin.jsx
+++ src/pages/ParentLogin/ParentLogin.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './parentLogin.module.css'
 import loginImage from '../../assets/images/cuate.png';
-
+import logoImage from '../../assets/images/signInLogo.png';
 
 function ParentLogin() {
 //   const handleLogin = (e) => {
@@ -18,36 +18,40 @@ function ParentLogin() {
 //   };
 
   return (
-    <div className= {styles.mainContainer}>
-      <div className= {styles.imageArea}>
-        <img src={loginImage} alt="Log in Image"  className= {styles.loginImage} />
-      </div>
-      <div className= {styles.formArea}>
-        <div className= {styles.formContainer}>
-          <h2>Welcome</h2>
-          <h4>Please enter your information below to resume your beautiful learning experience.</h4>
-          <form>
-            <div>
-              <input type="text" id="email" placeholder='Email/Username' required/>
-            </div>
-            <div>
-              <input type="password" id="password" placeholder='Password' required/>
-            </div>
-            <button type="submit">Sign In</button>
-          </form>
-          <div className= {styles.line}></div>
-          <h3>OR</h3>
-          <div>
-            <button className= {styles.googleBtn}>
-              Sign In with Google
-            </button>
-            <button className= {styles.facebookBtn}>
-              Sign In with Facebook
-            </button>
-          </div>
-          <p className= {styles.signUp}>Don’t have an account? <a>Sign up</a> </p>
+    <div className= {styles.mainBody}>
+      {/* <img src={logoImage} alt="Logo Image" lassName= {styles.logoImage} /> */}
+      <div className= {styles.mainContainer}>
+        <div className= {styles.imageArea}>
+        <img src={logoImage} alt="Logo Image" lassName= {styles.logoImage} />
+          <img src={loginImage} alt="Log in Image"  className= {styles.loginImage} />
         </div>
-      </div>  
+        <div className= {styles.formArea}>
+          <div className= {styles.formContainer}>
+            <h2>Welcome</h2>
+            <h4>Please enter your information below to resume your beautiful learning experience.</h4>
+            <form>
+              <div>
+                <input type="text" id="email" placeholder='Email/Username' required/>
+              </div>
+              <div>
+                <input type="password" id="password" placeholder='Password' required/>
+              </div>
+              <button type="submit">Sign In</button>
+            </form>
+            <div className= {styles.line}></div>
+            <h3>OR</h3>
+            <div>
+              <button className= {styles.googleBtn}>
+                Sign In with Google
+              </button>
+              <button className= {styles.facebookBtn}>
+                Sign In with Facebook
+              </button>
+            </div>
+            <p className= {styles.signUp}>Don't have an account? <a>Sign up</a> </p>
+          </div>
+        </div>  
+      </div>
     </div>
   );
 }
