b16f36209b923137f7def4b15b0d68688ba6adb7
Pricelesssarah-ui <pricelesssarah3@gmail.com>
2023-05-24 22:24:30 +0100

    AdminLoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import StudentDashboard from "./pages/StudentDashboard/StudentDashboard";
+import StudentDashboard from "./Pages/StudentDashboard/StudentDashboard";
+import AdminPage from "./Pages/AdminLogin/adminLogin";
+import ForgotPasswordAdmin from "./Pages/ForgotPasswordAdmin/ForgotPasswordAdmin";
 
 function App() {
   return (
     <div>
       <StudentDashboard />
+      <AdminPage />
+      <ForgotPasswordAdmin />
     </div>
   )
 }
