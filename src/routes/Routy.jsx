8bb6f6ab74595bb20096a714d3afb7dbf25d676d
bukason20 <chukwuebukaedward20@gmail.com>
2023-05-25 18:11:58 +0100

    modified the paths for the login and signup pages

--- src/routes/Routy.jsx
+++ src/routes/Routy.jsx
@@ -8,10 +8,11 @@ import StudentSidebar from "../components/StudentSideBar/StudentSidebar";
 import AdminLogin from "../pages/AdminLogin/AdminLogin";
 import LandingPage from "../pages/LandingPage/LandingPage";
 import ParentLogin from "../pages/ParentLogin/ParentLogin";
-import SignUp from "../pages/SignUp/SignUp";
+import SignUp from "../pages/StudentSignUp/StudentSignUp";
 import SecondSignUp from "../pages/SecondSignUp/SecondSignUp";
 import StudentDashboard from "../pages/StudentDashboard/StudentDashboard";
 import SignUpOptions from "../pages/SignUpOptions/SignUpOptions";
+import StudentSignUp from "../pages/StudentSignUp/StudentSignUp";
 
     const Routy = () => {
     
@@ -21,7 +22,7 @@ import SignUpOptions from "../pages/SignUpOptions/SignUpOptions";
             <Route path='/' element= {<LandingPage />} />
             
             {/* DASHBOARD */}
-              <Route path = "dashboard" element = {<StudentSidebar />}> 
+              <Route path = "/student/dashboard" element = {<StudentSidebar />}> 
                 <Route index element = {<StudentDashboard />} />
               </Route>
              
@@ -29,13 +30,13 @@ import SignUpOptions from "../pages/SignUpOptions/SignUpOptions";
               <Route path = "/login" element = {<ParentLogin />} />
 
               {/* SIGN UP OPTIONS */}
-              <Route path = "/signupoptions" element = {<SignUpOptions />} />
+              <Route path = "/signup" element = {<SignUpOptions />} />
 
               {/* SIGN UP PAGE */}
-              <Route path = "/signup" element = {<SignUp />} />
+              <Route path = "/signup/student" element = {<StudentSignUp />} />
 
               {/* SECOND SIGN UP PAGE */}
-              <Route path = "/finishsignup" element = {<SecondSignUp />} />
+              <Route path = "/signup/student_2" element = {<SecondSignUp />} />
 
               {/* ADMIN PAGEs */}
               <Route path = "/adminLogin" element = {<AdminLogin />} />
