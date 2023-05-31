dceb8700ec6b647ecefe8164d1471937f9e0a2b0
bukason20 <chukwuebukaedward20@gmail.com>
2023-05-31 11:38:07 +0100

    modified the sign up pages

--- src/pages/StudentSignUp/StudentSignUp.jsx
+++ src/pages/StudentSignUp/StudentSignUp.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import  React, {useContext, useState } from 'react';
 import styles from './studentsignup.module.css'
 import {Logo, FormImg, ArrowRight} from '../../assets';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
+import AppContext from "../../context/Appcontext"
 
 
 function StudentSignUp() {
+  const [parentName, setParentName] = useState("")
+  const [parentEmail, setParentEmail] = useState("")
+  const [parentRelationship, setParentRelationship] = useState("")
+  const { studentData} = useContext(AppContext)
+  const navigate = useNavigate() 
+  const submitParentInfo = (e) => {
+    e.preventDefault();
+
+    studentData.parentName = parentName
+    studentData.parentEmail = parentEmail
+    studentData.parentRelationship = parentRelationship;
+
+    console.log(studentData)
+    navigate("/signup/student_2")
+
+  }
     return (
         <div className= {styles.mainBody}>
         <div className= {styles.mainContainer}>
@@ -17,25 +34,25 @@ function StudentSignUp() {
               <h2 className = {styles.h2}>Sign up</h2>
               <h4 className = {styles.h4}>Please enter the information below to give your child an edge.</h4>
               <p className= {styles.formTitle}>Parents Information</p>
-              <form className = {styles.form}>
+              <form className = {styles.form} onSubmit = {submitParentInfo}>
                 <div>
                   <label>Name</label>
-                  <input type="text" id="name" placeholder='Full name' required/>
+                  <input type = "text" value = {parentName} id="name" placeholder='Full name'  onChange = {(e) => setParentName(e.target.value)} required/>
                 </div>
                 <div>
                   <label>Email address</label>  
-                  <input type="email" id="email" placeholder='Ifunanya123@gmail.com' required/>
+                  <input value = "email" value =  {parentEmail} id="email" placeholder='Ifunanya123@gmail.com' onChange = {(e) => setParentEmail(e.target.value)} required/>
                 </div>
                 <div>
                   <label>Relationship</label>  
-                  <input type="text" id="relationship" placeholder='Mother' required/>
+                  <input type = "text"  value = {parentRelationship} id="relationship" placeholder='Mother'  onChange = {(e) => setParentRelationship(e.target.value)} required/>
                 </div>
+                <button type = "submit" className= {styles.continue} >
+                  <p>Continue</p>
+                  <img src= {ArrowRight} alt=""/>
+                </button>
                 
               </form>
-              <NavLink to = "/signup/student_2" className= {styles.continue} >
-                <p>Continue</p>
-                <img src= {ArrowRight} alt=""/>
-              </NavLink>
               
             <div>
                 
