3775c502efd95e0694a8e7525c0b3d8cbe15aa73
Ezike Nkemdili Jane <ezikenkemdilijane@gmail.com>
2023-06-01 14:56:04 +0100

    testing branch

--- src/components/StudentSideBar/StudentSidebar.jsx
+++ src/components/StudentSideBar/StudentSidebar.jsx
@@ -1,6 +1,17 @@
-import styles from "./studentSidebar.module.css"
-import { LogoSvg, ProfileImg, Dashboard, Quiz, Resources, Assignments, Messages, Timetable, 
-    Settings, FAQ, Support } from "../../assets"
+import styles from "./studentSidebar.module.css";
+import {
+  LogoSvg,
+  ProfileImg,
+  Dashboard,
+  Quiz,
+  Resources,
+  Assignments,
+  Messages,
+  Timetable,
+  Settings,
+  FAQ,
+  Support,
+} from "../../assets";
 import { useState } from "react";
 import { Outlet } from "react-router";
 import { NavLink } from "react-router-dom";
@@ -33,109 +44,106 @@ function StudentSidebar() {
           }
         }
       });
-    }}
-    return (
-        <div className = {styles.dashboardContainer}>
-            <div className= {styles.dashboardNav}>
-                <div className= {styles.logo}>
-                    <img src= {LogoSvg} alt=""/>
-                    <h3>Learn.Z</h3>
-                </div>
-                
-                <div className= {styles.lowerContents}>
-                    <div className= {styles.info}>
-                        <img src= {ProfileImg} alt=""/>
-                        <p id = {styles.name}>Emily Johnson</p>
-                        <p id = {styles.level}> Level 2</p>
-                    </div>
-                    
-                    <div className= {styles.stats}>
-                        <div className= {styles.stat}>
-                            <p>Badges</p>
-                            <p id = {styles.statNo}>5</p>
-                        </div>
-                        <div className= {styles.stat}>
-                            <p>Position</p>
-                            <p id = {styles.statNo}>2nd</p>
-                        </div>
-                        <div className= {styles.stat}>
-                            <p>Friends</p>
-                            <p id = {styles.statNo}>23</p>
-                        </div>
-                    </div>
+    }
+  };
+  return (
+    <div className={styles.dashboardContainer}>
+      <div className={styles.dashboardNav}>
+        <div className={styles.logo}>
+          <img src={LogoSvg} alt="" />
+          <h3>Learn.Z</h3>
+        </div>
 
-                <div className= {styles.menu}>
-                    <ul className = {styles.links} onClick = {activeLink}>
-                        <li className = {styles.link}>
-                            <div className={`${styles.linkImg} ${styles.firstImg}`}>
-                                <img src= {Dashboard} alt=""/>
-                            </div>
-                            
-                            <p>Dashboard</p>
-                        </li>
-                        <li className = {styles.link}>
-                            <div className={styles.linkImg}>
-                                <img src= {Quiz} alt=""/>
-                            </div>
-                           <p>Quiz</p>
-                        </li>
-                        <li className = {styles.link}>
-                            <div className={styles.linkImg}>
-                                <img src= {Resources} alt=""/>
-                            </div>
-                            <p>Resources</p>
-                        </li>
-                        <li className = {styles.link}>
-                            <div className={styles.linkImg}>
-                                <img src= {Assignments} alt=""/>
-                            </div>
-                           <p>Assignments</p>
-                        </li>
-                        <li className = {styles.link}>
-                            <div className={styles.linkImg}>
-                                <img src= {Messages} alt=""/>
-                            </div>
-                             <p>Messages</p>
-                        </li>
-                        <li className = {styles.link}>
-                            <div className={styles.linkImg}>
-                                <img src={Timetable} alt=""/>
-                            </div>
-                            <p>TimeTable</p> 
-                        </li>
-                        <li className = {styles.link}>
-                            <div className={styles.linkImg}>
-                                <img src= {Settings} alt=""/>
-                            </div>
-                            <p>Settings</p> 
-                        </li>
-                        <p id = {styles.haveQuestion}>Have a question</p>
-                        <li className = {styles.link}>
-                            <div className={styles.linkImg}>
-                                <img src= {FAQ} alt=""/>
-                            </div>
-                            <p>FAQ</p> 
-                        </li>
-                        <li className = {styles.link}>
-                            <div className={styles.linkImg}>
-                                <img src={Support} alt=""/>
-                            </div>
-                            <p>Support</p>
-                        </li>
-                        <li className = {styles.link}>
-                        </li>
-                    </ul>
-                </div>
-                
+        <div className={styles.lowerContents}>
+          <div className={styles.info}>
+            <img src={ProfileImg} alt="" />
+            <p id={styles.name}>Emily Johnson</p>
+            <p id={styles.level}> Level 2</p>
+          </div>
+
+          <div className={styles.stats}>
+            <div className={styles.stat}>
+              <p>Badges</p>
+              <p id={styles.statNo}>5</p>
             </div>
-            <main>
-                <Outlet />
-            </main>
-            
-            
+            <div className={styles.stat}>
+              <p>Position</p>
+              <p id={styles.statNo}>2nd</p>
+            </div>
+            <div className={styles.stat}>
+              <p>Friends</p>
+              <p id={styles.statNo}>23</p>
+            </div>
+          </div>
+
+          <div className={styles.menu}>
+            <ul className={styles.links} onClick={activeLink}>
+              <li className={styles.link}>
+                <div className={`${styles.linkImg} ${styles.firstImg}`}>
+                  <img src={Dashboard} alt="" />
+                </div>
+
+                <NavLink to="/student/dashboard">Dashboard</NavLink>
+              </li>
+              <li className={styles.link}>
+                <div className={styles.linkImg}>
+                  <img src={Quiz} alt="" />
+                </div>
+                <NavLink to="studentQuiz">Quiz</NavLink>
+              </li>
+              <li className={styles.link}>
+                <div className={styles.linkImg}>
+                  <img src={Resources} alt="" />
+                </div>
+                <p>Resources</p>
+              </li>
+              <li className={styles.link}>
+                <div className={styles.linkImg}>
+                  <img src={Assignments} alt="" />
+                </div>
+                <p>Assignments</p>
+              </li>
+              <li className={styles.link}>
+                <div className={styles.linkImg}>
+                  <img src={Messages} alt="" />
+                </div>
+                <NavLink to="studentMessage">Message</NavLink>
+              </li>
+              <li className={styles.link}>
+                <div className={styles.linkImg}>
+                  <img src={Timetable} alt="" />
+                </div>
+                <p>TimeTable</p>
+              </li>
+              <li className={styles.link}>
+                <div className={styles.linkImg}>
+                  <img src={Settings} alt="" />
+                </div>
+                <p>Settings</p>
+              </li>
+              <p id={styles.haveQuestion}>Have a question</p>
+              <li className={styles.link}>
+                <div className={styles.linkImg}>
+                  <img src={FAQ} alt="" />
+                </div>
+                <p>FAQ</p>
+              </li>
+              <li className={styles.link}>
+                <div className={styles.linkImg}>
+                  <img src={Support} alt="" />
+                </div>
+                <p>Support</p>
+              </li>
+              <li className={styles.link}></li>
+            </ul>
+          </div>
         </div>
-        </div>
-    )
+      </div>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
 }
 
 export default StudentSidebar;
