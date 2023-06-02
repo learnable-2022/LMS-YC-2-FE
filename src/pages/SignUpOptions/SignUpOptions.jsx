2e5635fb75f01063edc68b51ddf99cfafe17b012
bukason20 <chukwuebukaedward20@gmail.com>
2023-06-02 01:24:04 +0100

    modified the parent login and sign up option pages

--- src/pages/SignUpOptions/SignUpOptions.jsx
+++ src/pages/SignUpOptions/SignUpOptions.jsx
@@ -17,36 +17,33 @@ function SignUpOptions() {
   }
 
   return (
-    <div className= {styles.mainBody}>
-      <div className= {styles.mainContainer}>
-        <div className= {styles.imageArea}>
-          <img src={Logo} alt="Logo Image" className= {styles.logoImage} />
-          <img src={FormImg} alt="Log in Image"  className= {styles.loginImage} />
-        </div>
-        <div className= {styles.formArea}>
-          <div className= {styles.formContainer}>
-            <h2 className = {styles.h2}>Sign up</h2>
-            <h4 className = {styles.h4}>Please enter the information below to give your child an edge.</h4>
-            <p className= {styles.formTitle}>Please select an appropriate category</p>
-            <div>
-            <button to = "/signup/student" className= {`${styles.signupOption} ${pathLink == "student" ? styles.active : ""}`} onClick = {goToStudent}>
-              <img src= {ProfileIcon} alt=""/>
-              <p> Sign up as a student</p>
-            </button>
-            <button to = "/signup" type="submit" className= {`${styles.signupOption} ${pathLink == "tutor" ? styles.active : ""}`} onClick = {goToTutor}>
-              <img src= {ProfileIcon} alt=""/>
-              <p> Sign up as a tutor</p>
-            </button>
-            <NavLink to = {pathLink == "none" ? "/signup" : pathLink == "student" ? "/signup/student" : "/admin/signup" } className= {`${styles.continue} ${pathLink == "none" ? styles.disabled : ""}`}>
-              <p>Continue</p>
-              <img src= {ArrowRight} alt=""/>
-            </NavLink>
-            </div>
-          <p className= {styles.login}>Already have an account? <NavLink to = "/login">Log in</NavLink> </p>
-          </div>
-        </div>  
+    <div className= {styles.signupOtionsContainer}>
+      <div className= {styles.imageArea}>
+        <img src={FormImg} alt="Log in Image"  className= {styles.loginImage} />
       </div>
+      <div className= {styles.formContainer}>
+        <h2>Sign up</h2>
+        <h4>Please enter the information below to give your child an edge.</h4>
+        <p className= {styles.formTitle}>Please select an appropriate category</p>
+        <div className = {styles.options}>
+          <button to = "/signup/student" className= {`${styles.signupOption} ${pathLink == "student" ? styles.active : ""}`} onClick = {goToStudent}>
+            <img src= {ProfileIcon} alt=""/>
+            <p> Sign up as a student</p>
+          </button>
+          <button to = "/signup" type="submit" className= {`${styles.signupOption} ${pathLink == "tutor" ? styles.active : ""}`} onClick = {goToTutor}>
+            <img src= {ProfileIcon} alt=""/>
+            <p> Sign up as a tutor</p>
+          </button>
+          <NavLink to = {pathLink == "none" ? "/signup" : pathLink == "student" ? "/signup/student" : "/admin/signup" } className= {`${styles.continue} ${pathLink == "none" ? styles.disabled : ""}`}>
+            <p>Continue</p>
+            <img src= {ArrowRight} alt=""/>
+          </NavLink>
+        </div>
+        <p className= {styles.login}>Already have an account? <NavLink to = "/login">Log in</NavLink> </p>
+      </div>
+        
     </div>
+    
   )
 }
 
