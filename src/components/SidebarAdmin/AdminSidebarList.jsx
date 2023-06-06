fca14aac199c24b362f46088d1bfdd54ea7df9fc
bukason20 <chukwuebukaedward20@gmail.com>
2023-06-06 18:13:51 +0100

    renamed the admin sidebar

---
+++ src/components/SidebarAdmin/AdminSidebarList.jsx
@@ -0,0 +1,67 @@
+import { BsColumnsGap, BsFillCollectionFill } from "react-icons/bs";
+import { FaBookReader } from "react-icons/fa";
+import { MdAssignment, MdOutlineSettingsSuggest } from "react-icons/md";
+import { TiMessages } from "react-icons/ti";
+import { FcOvertime } from "react-icons/fc";
+import { VscReport } from "react-icons/vsc";
+
+
+
+
+
+const AdminSidebarList = [
+    {
+        id: 1,
+        logo: <BsColumnsGap/>,
+        name: 'Home',
+        link: "/admin/dashboard"
+    },
+    {
+        id: 2,
+        logo: <FaBookReader/>,
+        name: 'Students',
+        link: "/admin/dashboard"
+    },
+    {
+        id: 3,
+        logo: <BsFillCollectionFill/>,
+        name: 'Lessons',
+        link: "lessons"
+    },
+    {
+        id: 4,
+        logo: <MdAssignment/>,
+        name: 'Assignments',
+        link: "/admin/dashboard"
+    },
+    {
+        id: 5,
+        logo: <TiMessages/>,
+        name: 'Messages',
+        link: "/admin/dashboard"
+    },
+    {
+        id: 6,
+        logo: <FcOvertime/>,
+        name: 'Timetable',
+        link: "/admin/dashboard"
+    },
+    {
+        id: 7,
+        logo: <VscReport/>,
+        name: 'Reports',
+        link: "lessons"
+    },
+    {
+        id: 8,
+        logo: <MdOutlineSettingsSuggest/>,
+        name: 'Settings',
+        link: "/admin/dashboard"
+    },{
+        id: "",
+        logo: "",
+        name: ""
+    }
+]
+
+export default AdminSidebarList;
