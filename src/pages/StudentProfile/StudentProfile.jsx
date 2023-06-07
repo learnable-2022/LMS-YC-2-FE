a079ccee1558720850eb47c6d21179fdab87bc19
bukason20 <chukwuebukaedward20@gmail.com>
2023-06-08 00:28:38 +0100

    modified the student pages and their styles

--- src/pages/StudentProfile/StudentProfile.jsx
+++ src/pages/StudentProfile/StudentProfile.jsx
@@ -1,4 +1,5 @@
 import { useContext, useEffect } from 'react'
+import UserDashboardNav from '../../components/UserDashboardNav/UserDashboardNav'
 import AppContext from '../../context/Appcontext'
 import styles from './studentProfile.module.css'
 
@@ -7,26 +8,25 @@ import styles from './studentProfile.module.css'
 function StudentProfile() {
     const {studentInfo} = useContext(AppContext)
 
-    const childDOB = new Date(studentInfo.user[0].DOB)
+    const childDOB = new Date(studentInfo.DOB)
 
     useEffect(() =>{
         console.log(studentInfo)
     })
     return (
-        <div>
-            <h2>Student Profile</h2>
-
-            <p>Student Name : {studentInfo.user[0].child_name}</p>
-            <p>Gender : {studentInfo.user[0].child_name}</p>
-            <p>Email : {studentInfo.user[0].child_name}</p>
-            <p>Student DOB : {childDOB.getFullYear()}</p>
-            <p>Parent Name : {studentInfo.user[0].parent_name}</p>
-            <p>Parent Relations : {studentInfo.user[0].relationship}</p>
-            <p>Student Name : {studentInfo.user[0].child_name}</p>
-            <p>Student Name : {studentInfo.user[0].child_name}</p>
-            <p>Student Name : {studentInfo.user[0].child_name}</p>
-            <p>Student Name : {studentInfo.user[0].child_name}</p>
-            <p>Student Name : {studentInfo.user[0].child_name}</p>
+        <div className = {styles.container}>
+            <UserDashboardNav navTitle = "Profile"/>
+    
+            <div className= {styles.body}>
+                <p>Student Name : {studentInfo.child_name}</p>
+                <p>Gender : {studentInfo.gender}</p>
+                <p>Email : {studentInfo.email}</p>
+                <p>Student DOB : {studentInfo.DOB}</p>
+                <p>Parent Name : {studentInfo.parent_name}</p>
+                <p>Parent Relations : {studentInfo.relationship}</p>
+                <p>Path : {studentInfo.path == null || studentInfo.path == undefined || studentInfo.path == "NULL " ? "" : studentInfo.path}</p>
+            </div>
+            
         </div>
     )
 }
