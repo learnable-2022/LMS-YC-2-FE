0b3615da9d972c65813e66e0c487f20a3c4b8f7d
bukason20 <chukwuebukaedward20@gmail.com>
2023-06-25 19:34:00 +0100

    modified the admin dashboard

--- src/components/AdminDashboardNav/AdminDashboardNav.jsx
+++ src/components/AdminDashboardNav/AdminDashboardNav.jsx
@@ -1,9 +1,26 @@
 import styles from "./adminDashboardNav.module.css"
+import { FiMenu } from "react-icons/fi"
+import { LogoSvg , Logo} from "../../assets"
+import { useContext, useState } from "react"
+import AppContext from "../../context/Appcontext"
 
 function AdminDashboardNav({navTitle}) {
+    const { adminInfo, showAdminNav, setShowAdminNav } = useContext(AppContext)
+   
     return (
         <nav className = {styles.container}>
-            <h3>{navTitle}</h3>
+            <h3 className = {styles.title}>{navTitle}</h3>
+            <div className= {styles.burger} onClick = {() => setShowAdminNav(true)}>
+                <FiMenu />
+            </div>
+            <div className= {styles.logo}>
+                <img src= {LogoSvg} alt=""/>
+                <p>LearnZ</p>
+            </div>
+
+            <div className= {styles.navName}>
+                <p>{adminInfo.email[0].toUpperCase()}</p>
+            </div>
         </nav>
     )
 }
