a395503724912966e187768cd2dfa8dfae83cb25
Bukason20 <chukwuebukedward20@gmail.com>
2023-06-26 01:15:23 +0100

    modified some pages

--- src/components/StudentStats/StudentStats.jsx
+++ src/components/StudentStats/StudentStats.jsx
@@ -4,32 +4,34 @@ import Card from "../Card/Card"
 import styles from "./studentStats.module.css"
 
 function StudentStats() {
-  const {progress, totalVideos, scorePerentage} = useContext(AppContext)
+  const { progress, totalVideos, scorePerentage } = useContext(AppContext)
   const progressPercentage = (progress / totalVideos) * 100
-    return (
-      <div className= {styles.studentStatsContainer}>
-        <div className= {styles.studentStats}>
-          <Card bgColor = "#FEEDCF">
-            <p>Your progress</p>
 
-            <h2>{isNaN(progressPercentage) ? 0 + "%" : progressPercentage.toFixed(0) + "%"}</h2>
-          </Card>
+  console.log(scorePerentage)
+  return (
+    <div className={styles.studentStatsContainer}>
+      <div className={styles.studentStats}>
+        <Card bgColor="#FEEDCF">
+          <p>Your progress</p>
 
-          <Card bgColor = "#FEEDCF">
-            <p>Badges Earned</p>
+          <h2>{isNaN(progressPercentage) ? 0 + "%" : progressPercentage.toFixed(0) + "%"}</h2>
+        </Card>
 
-            <h2>0</h2>    
-          </Card>
+        <Card bgColor="#FEEDCF">
+          <p>Badges Earned</p>
 
-          <Card bgColor = "#FEEDCF">
-            <p>Total Quiz Score</p>
+          <h2>0</h2>
+        </Card>
 
-            <h2>{scorePerentage ? scorePerentage : 0 + "%"}</h2>
-          </Card>
-        </div>
+        <Card bgColor="#FEEDCF">
+          <p>Total Quiz Score</p>
+
+          <h2>{scorePerentage ? scorePerentage : 0 + "%"}</h2>
+        </Card>
       </div>
-        
-    )
+    </div>
+
+  )
 }
 
 export default StudentStats
--- src/pages/Admin/LessonOverview/LessonOverview.jsx
+++ src/pages/Admin/LessonOverview/LessonOverview.jsx
@@ -5,108 +5,112 @@ import { NavLink, useLocation } from 'react-router-dom';
 import AppContext from '../../../context/Appcontext';
 import { CourseLoader } from "../../../assets"
 import ThumbnailGenerator from './VideoThumbnail';
-import {AiFillDelete} from "react-icons/ai"
-import {BsFillPersonFill} from "react-icons/bs"
+import { AiFillDelete } from "react-icons/ai"
+import { BsFillPersonFill } from "react-icons/bs"
 
 function LessonOverview() {
   const [allCourses, setAllCourses] = useState()
   const [myCourses, setMyCourses] = useState()
   const [loading, setLoading] = useState(false)
   const location = useLocation()
-  const {adminToken, adminInfo } = useContext(AppContext)
+  const { adminToken, adminInfo } = useContext(AppContext)
+  const [deletedCourseId, setDeletedCourseId] = useState(null);
+
   // console.log(location)
 
   const getAllCourses = () => {
     setLoading(true)
     const response = fetch('https://learnz.onrender.com/api/v1/admin/courses', {
       headers: {
-        "Content-Type" : "application/json",
-        "Authorization" : `Bearer ${adminToken}`
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${adminToken}`
       }
     })
-    .then(res => res.json())
-    .then(data => {
-      console.log(data.courses)
-      setLoading(false)
-      data.success? setAllCourses(data.courses) : ""
-      data.success ? setMyCourses(data.courses.filter((course, index) => course.admin._id == adminInfo._id )) : ""
-    })
-    .catch((err) => {
-      setLoading(false)
-      console.log(err)
-    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.courses)
+        setLoading(false)
+        data.success ? setAllCourses(data.courses) : ""
+        data.success ? setMyCourses(data.courses.filter((course, index) => course.admin._id == adminInfo._id)) : ""
+      })
+      .catch((err) => {
+        setLoading(false)
+        console.log(err)
+      })
 
   };
-  const courseDelete = (id) => {
-    if(window.confirm("Are you sure you want to delete this course")){
+  const courseDelete = (id, title) => {
+    if (window.confirm(`Are you sure you want to delete this course ${title}`)) {
       const response = fetch(`https://learnz.onrender.com/api/v1/admin/courses/${id}`, {
-      method: "DELETE",  
-      headers : {
-        "Authorization" : `Bearer ${adminToken}`
-      }
-    })
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
-      if(data.success){
-        window.alert(data.message)
-      }
-    })
+        method: "DELETE",
+        headers: {
+          "Authorization": `Bearer ${adminToken}`
+        }
+      })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+          if (data.success) {
+            window.alert(data.message)
+            setDeletedCourseId(id);
+          }
+        })
     }
-    
+
   }
-  
+
   useEffect(() => {
     getAllCourses()
-   }, [courseDelete])
+  }, [deletedCourseId])
 
 
-    return (
-    <div className = {styles.mainbar}>
-      <AdminDashboardNav navTitle = "Lessons" />
-      <div className= {styles.mainbody}>
-        <NavLink to = "/admin/dashboard/createLesson" className= {styles.mainnewlesson}>
+  return (
+    <div className={styles.mainbar}>
+      <AdminDashboardNav navTitle="Lessons" />
+      <div className={styles.mainbody}>
+        <NavLink to="/admin/dashboard/createLesson" className={styles.mainnewlesson}>
           Create new Lesson
         </NavLink>
 
-        <div className= {styles.allCourses}>
+        <div className={styles.allCourses}>
           <h2>All Courses</h2>
-          
+
           {loading ? (
-            <div className = {styles.coursesLoading}>
-              <img src= {CourseLoader} alt=""/>
+            <div className={styles.coursesLoading}>
+              <img src={CourseLoader} alt="" />
             </div>
-          ): (
-            <div className = {styles.coursesCont}>
-              {allCourses !== null && allCourses !== undefined ? allCourses.map((course, index)  => (
-                <div className ={styles.course} key ={index}>
-                  <div className= {styles.thumbNailImg}>
-                    <ThumbnailGenerator videoUrl = {course.url} />
+          ) : (
+            <div className={styles.coursesCont}>
+              {allCourses !== null && allCourses !== undefined ? allCourses.map((course, index) => (
+                <div className={styles.course} key={index}>
+                  <div className={styles.thumbNailImg}>
+                    <ThumbnailGenerator videoUrl={course.url} />
                   </div>
-                  <div className= {styles.courseInfo}>
-                    <p id= {styles.courseTitle}>{course.title}</p>
+                  <div className={styles.courseInfo}>
+                    <p id={styles.courseTitle}>{course.title}</p>
+                    <p>{course.track}</p>
                     <div className={styles.icons}>
-                      <div className= {styles.admin}>
+                      <div className={styles.admin}>
                         <BsFillPersonFill />
                         <p>{course.admin.email}</p>
                       </div>
                       {course.admin._id == adminInfo._id ? (
-                        <div className= {styles.deleteCourse} onClick = {() => courseDelete(course._id)}>
+                        <div className={styles.deleteCourse} onClick={() => courseDelete(course._id, course.title)}>
                           <AiFillDelete />
-                        </div> ) : ""}
+                        </div>) : ""}
                     </div>
-                    
+
                   </div>
-                  
+
                 </div>
-                )) : (
-                  <div>No course</div>
-                )}
+              )) : (
+                <div>No course</div>
+              )}
             </div>
-            
+
           )}
         </div>
-        
+
       </div>
     </div>
   )
